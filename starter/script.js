dd61399d8814fb0ddcc20271edaabfe410a08121
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-09-23 20:41:53 +0100

    commit

--- starter/script.js
+++ starter/script.js
@@ -63,35 +63,35 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
 
-  request.send();
-  // console.log(request.responseText);
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    renderCountry(data);
+//   request.send();
+//   // console.log(request.responseText);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     renderCountry(data);
 
-    //Get neighbouring contry2
+//     //Get neighbouring contry2
 
-    const neighbour = data.borders?.[0];
-    if (!neighbour) return;
+//     const neighbour = data.borders?.[0];
+//     if (!neighbour) return;
 
-    // Ajax  call 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
+//     // Ajax  call 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
-getCountryData('usa');
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+// getCountryData('usa');
 // getCountryData('Ghana');
 // getCountryData('usa');
 
@@ -111,5 +111,54 @@ getCountryData('usa');
 
 // request.send();
 
-const fetchh = fetch(`https://restcountries.com/v2/name/Nigeria`);
-console.log(fetchh);
+// const fetchh = fetch(`https://restcountries.com/v2/name/Nigeria`);
+// console.log(fetchh);
+
+///////////////////////Fetch data using a promise////////////////////////
+
+// const getCountryData = function (country) {
+//   //the first step is the fetch method returning a promise and we use the then method to handle the
+//   //promise using the then method, inorder to read the data from that response we then call the jason method response
+//   // methon on that response metho
+//   fetch(`https://restcountries.com/v2/name/ ${country}`)
+//     //this will also return a new promise that we need to handle
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/ ${country}`)
+//     .then(function (response) {
+
+//       return response.json();
+//     })
+//     .then(function (data) {
+
+//       renderCountry(data[0]);
+//     });
+// };
+// getCountryData('Nigeria');
+
+///////////Modifying the code///////////////////////////
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) return;
+
+      // country2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+getCountryData('Nigeria');
+getCountryData('cameroon');
