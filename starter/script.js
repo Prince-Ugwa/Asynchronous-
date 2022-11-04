2a7ccf9e71cd3f75cb31ee727712b866913d84f9
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-11-04 21:58:40 +0100

    commit

--- starter/script.js
+++ starter/script.js
@@ -452,5 +452,12 @@ btn.addEventListener('click', () => {
 //////////////////EVENT LOOP IN PRACTICE////////////////////////
 console.log(`Test start`);
 setTimeout(() => console.log('0 sec timer'), 0);
-Promise.resolve(`Resolved promise`).then(res => console.log(res));
+Promise.resolve(`Resolved promise 1`).then(res => console.log(res));
+
+Promise.resolve(`Promise 2`).then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
 console.log(`Test end`);
+
+//BUILDING A SIMPLE FUNCTION,////////////////
