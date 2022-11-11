75ef1e7663ba1bed7a7e6ce45b08c5de08adcd7b
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-11-11 21:17:01 +0100

    commit

--- starter/script.js
+++ starter/script.js
@@ -918,36 +918,36 @@ const createImage = function (imgPath) {
 //   .then(() => (curImage.style.display = 'none'))
 //   .catch(err => console.error(err));
 
-const loadNpause = async function () {
-  try {
-    let img = await createImage(`img/img-1.jpg`);
-    console.log('image 1 loaded');
-    await wait(2);
-    img.style.display = 'none';
+// const loadNpause = async function () {
+//   try {
+//     let img = await createImage(`img/img-1.jpg`);
+//     console.log('image 1 loaded');
+//     await wait(2);
+//     img.style.display = 'none';
 
-    //load 2
-    img = await createImage(`img/img-2.jpg`);
-    console.log('image 2 loaded');
-    await wait(2);
-    img.style.display = 'none';
-  } catch (err) {
-    console.error(err);
-  }
-};
-// loadNpause();
+//     //load 2
+//     img = await createImage(`img/img-2.jpg`);
+//     console.log('image 2 loaded');
+//     await wait(2);
+//     img.style.display = 'none';
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// // loadNpause();
 
-//part
+// //part2
 
-const loadAll = async function (imgArr) {
-  try {
-    const imgs = imgArr.map(async img => await createImage(img));
-    // console.log(imgs);
+// const loadAll = async function (imgArr) {
+//   try {
+//     const imgs = imgArr.map(async img => await createImage(img));
+//     // console.log(imgs);
 
-    const imgEl = await Promise.all(imgs);
-    console.log(imgEl);
-    imgEl.forEach(img => img.classList.add('parallel'));
-  } catch (err) {
-    console.error(err);
-  }
-};
-loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+//     const imgEl = await Promise.all(imgs);
+//     console.log(imgEl);
+//     imgEl.forEach(img => img.classList.add('parallel'));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
